f3836fc2c8bfdfad59fd23e1fdaf79e43c11e803
askbur1 <askbur1@5CD82155G3.thn.local>
2019-09-06 10:09:25 +0200

    Start of project.

---
+++ prog1askan/src/prog1askan/Test1.java
@@ -0,0 +1,24 @@
+package prog1askan;
+
+public class Test1 {
+
+	public static void main(String [] arg) {
+		int y;//deklarerar variabeln y
+	    y=1; // tilldelar 1 till variablen y
+		int x=1; // deklarering + tilldelning=initsiering
+		// intigers=heltal
+		double tal3=2; 
+		String mening="hej asdasfasdasdasd";
+		char ettTecken;// läxa exempler 21-29 och 28.
+		boolean logik= false;
+		int antalhjul=2,  antalvexlar=3;
+	    System.out.println(tal3);
+		
+		
+	   
+	   
+	   
+	}
+	
+	
+}
